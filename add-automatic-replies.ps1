a6e1b45f4878c38f2e4b0b5e9e9ecdcdcd0e39e3
Chris Mendez <chrism@knightoffice.com>
2022-09-28 16:57:06 -0500

    update

--- add-automatic-replies.ps1
+++ add-automatic-replies.ps1
@@ -1,11 +1,22 @@
-Connect-ExchangeOnline
-$InternMsg = "Hello,
-`Please note, this is an unmonitored mailbox.
-`If you need IT assistance, please email support@knightoffice.com. For more urgent issues, please call us at (210) 340-8909 and one of our dedicated Service Desk Technicians will assist.
-`Thank you,
-`Knight Office Solutions
-`IT Support
-"
 
-Set-MailboxAutoReplyConfiguration -Identity knightms -AutoReplyState Enabled -InternalMessage $InternMsg -ExternalAudience None
-Disconnect-ExchangeOnline
+Connect-ExchangeOnline -InlineCredential
+try {
+    Get-Mailbox knightms
+    $InternMsg = "Hello,<br />
+    <br />
+    `nPlease note, this is an unmonitored mailbox.<br />
+    <br />
+    `nIf you test need IT assistance, please email support@knightoffice.com. For more urgent issues, please call us at (210) 340-8909 and one of our dedicated Service Desk Technicians will assist.<br />
+    <br />
+    `nThank you,<br />
+    <br />
+    `nKnight Office Solutions<br />
+    `nIT Support<br />
+    "
+    Set-MailboxAutoReplyConfiguration -Identity knightms -AutoReplyState Enabled -InternalMessage $InternMsg -ExternalAudience None
+}
+catch {Write-Host 'No knightms inbox '}
+finally {
+    Disconnect-ExchangeOnline
+}
+
