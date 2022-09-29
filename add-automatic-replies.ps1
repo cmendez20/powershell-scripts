0fe13e6dd80e2fa1a0129f72abbc0b8fbca78dc6
cmendez20 <cmendez20@utexas.edu>
2022-09-29 00:41:49 -0500

    Add array loop and credentials

--- add-automatic-replies.ps1
+++ add-automatic-replies.ps1
@@ -1,22 +1,39 @@
+$emails = Import-Excel .\customers.xlsx
 
-Connect-ExchangeOnline -InlineCredential
-try {
-    Get-Mailbox knightms
-    $InternMsg = "Hello,<br />
-    <br />
-    `nPlease note, this is an unmonitored mailbox.<br />
-    <br />
-    `nIf you test need IT assistance, please email support@knightoffice.com. For more urgent issues, please call us at (210) 340-8909 and one of our dedicated Service Desk Technicians will assist.<br />
-    <br />
-    `nThank you,<br />
-    <br />
-    `nKnight Office Solutions<br />
-    `nIT Support<br />
-    "
-    Set-MailboxAutoReplyConfiguration -Identity knightms -AutoReplyState Enabled -InternalMessage $InternMsg -ExternalAudience None
-}
-catch {Write-Host 'No knightms inbox '}
-finally {
-    Disconnect-ExchangeOnline
-}
+$emails.forEach(
+    {  
+      $credential = New-Object System.Management.Automation.PsCredential($_,$password)
+
+      try {
+
+        # Connect-ExchangeOnline -InlineCredential
+        # Connect-ExchangeOnline -UserPrincipalName $_
+        Connect-ExchangeOnline -Credential $credential
+        Get-Mailbox knightms
+        $InternMsg = "Hello,<br />
+        <br />
+        `nPlease note, this is an unmonitored mailbox.<br />
+        <br />
+        `nIf you test need IT assistance, please email support@knightoffice.com. For more urgent issues, please call us at (210) 340-8909 and one of our dedicated Service Desk Technicians will assist.<br />
+        <br />
+        `nThank you,<br />
+        <br />
+        `nKnight Office Solutions<br />
+        `nIT Support<br />
+        "
+        Set-MailboxAutoReplyConfiguration -Identity knightms -AutoReplyState Enabled -InternalMessage $InternMsg -ExternalAudience None
+        Write-Host "Knightms mailbox was found and automatic reply has been set."
+      }
+      
+      catch {
+        Write-Output 'FAILED: $($.Exception.Message)'
+        '`n$($_)' | Out-File C:\Users\Chris\errors.txt -Append
+    }
+      
+      finally {
+        Disconnect-ExchangeOnline
+      }
+  }
+)
+
 
