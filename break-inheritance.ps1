2f510f47f88798217e64bb75c6f128c9a7c74258
cmendez20 <cmendez20@utexas.edu>
2022-10-17 23:28:30 -0500

    updated with try/catch method, failure out-file, and more write-host

--- break-inheritance.ps1
+++ break-inheritance.ps1
@@ -3,7 +3,7 @@ $folderNames = Get-ChildItem -Directory | Select-Object -ExpandProperty Name
 
 # loops through all folders
 For ($i=0; $i -lt $folderNames.length; $i++) {
-  $folderPath = "\\dc\data\dataTwo\$($folderNames[$i])"
+  $folderPath = "\\dc\data\dataTen\$($folderNames[$i])"
   Write-Host "Removing inheritance for $($folderNames[$i])" -ForegroundColor cyan -BackgroundColor black
 
   # get current ACL for folder
@@ -12,36 +12,50 @@ For ($i=0; $i -lt $folderNames.length; $i++) {
   # disables inheritance
   $Acl.SetAccessRuleProtection($true,$true)
 
-  # filter for specific ACE you want to remove
+  # # filter for specific ACE you want to remove
   $AceToRemove = $Acl.Access | Where-Object {($_.IdentityReference -eq 'MYDOMAIN\Domain Users')}
   # $AceToRemove = New-Object System.Security.AccessControl.FileSystemAccessRule("MYDOMAIN\Domain Users", "FullControl", "Allow")
 
-  # # removes ACE
+  # # # removes ACE
   $Acl.RemoveAccessRule($AceToRemove)
 
-  # make changes stick
+  # # make changes stick
   Set-Acl -Path $folderPath -AclObject $Acl
 
   # adds domain user
   # Create the ACE for domain user
-  $identity = "$($folderNames[$i])"
-  $rights = 'Modify' #Other options: [enum]::GetValues('System.Security.AccessControl.FileSystemRights')
-  $inheritance = 'ContainerInherit, ObjectInherit' #Other options: [enum]::GetValues('System.Security.AccessControl.Inheritance')
-  $propagation = 'None' #Other options: [enum]::GetValues('System.Security.AccessControl.PropagationFlags')
-  $type = 'Allow' #Other options: [enum]::GetValues('System.Securit y.AccessControl.AccessControlType')
-  $ACE = New-Object System.Security.AccessControl.FileSystemAccessRule($identity,$rights,$inheritance,$propagation, $type)
 
-  $Acl = Get-Acl -Path "$folderPath"
-  $Acl.AddAccessRule($ACE)
+  try {
+    $identity = "$($folderNames[$i])"
+    $rights = 'Modify' #Other options: [enum]::GetValues('System.Security.AccessControl.FileSystemRights')
+    $inheritance = 'ContainerInherit, ObjectInherit' #Other options: [enum]::GetValues('System.Security.AccessControl.Inheritance')
+    $propagation = 'None' #Other options: [enum]::GetValues('System.Security.AccessControl.PropagationFlags')
+    $type = 'Allow' #Other options: [enum]::GetValues('System.Securit y.AccessControl.AccessControlType')
+    $ACE = New-Object System.Security.AccessControl.FileSystemAccessRule($identity,$rights,$inheritance,$propagation, $type)
+  
+    $Acl = Get-Acl -Path "$folderPath"
+    $Acl.AddAccessRule($ACE)
+  
+    Write-Host "Adding $($folderNames[$i]) domain account permissions" -ForegroundColor cyan -BackgroundColor black
+    Set-Acl -Path "$folderPath" -AclObject $Acl
+  }
+  catch {
+    Write-Host "$($folderNames[$i]) not found in active directory" -ForegroundColor red -BackgroundColor black
+    Write-Output "$($folderNames[$i])" | Out-File ./failed.txt
+  }
 
-  Set-Acl -Path "$folderPath" -AclObject $Acl
 
+     
+  icacls.exe $folderPath /remove "Domain Users" /T /C
+  # icacls.exe $folderPath /remove "Temporary 2 User" /T /C
+  # icacls.exe $folderPath /remove "Temporary 1 User" /T /C
+  Write-Host "Removing Everyone permissions group from $($folderNames[$i]) folder" -ForegroundColor cyan -BackgroundColor black
   icacls.exe $folderPath /remove Everyone /T /C
 }
 
 
 
-For ($i=0; $i -lt $folderNames.length; $i++) {
-  $folderPath = "\\dc\data\dataTwo\$($folderNames[$i])"
-  icacls.exe $folderPath /remove "Domain Users" /T /C
-}
+# For ($i=0; $i -lt $folderNames.length; $i++) {
+#   $folderPath = "\\dc\data\dataTwo\$($folderNames[$i])"
+#   icacls.exe $folderPath /remove "Domain Users" /T /C
+# }
