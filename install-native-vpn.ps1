d1188094860554d33c4ab6207262b701afb60ebb
Chris Mendez <chrism@knightoffice.com>
2022-12-15 16:05:53 -0600

    New script for offboard cloud 365 user

---
+++ install-native-vpn.ps1
@@ -0,0 +1,2 @@
+
+Add-VpnConnection -Name "DMCA VPN" -ServerAddress "199.227.124.194" -TunnelType "L2tp" -L2tpPsk "d0L0r05@" -AuthenticationMethod "Pap" -PassThru
---
+++ offboard-365-user.ps1
@@ -0,0 +1,57 @@
+#########User info######
+$userToOffboard = "Adriana@gastroclinicsa.com"
+# $CustomerDefaultDomainname = "gastroclinicsa.com"
+
+write-host "Logging into Azure AD." -ForegroundColor Green
+Connect-AzureAD
+
+write-host "Connecting to Exchange Online" -ForegroundColor Green
+Connect-ExchangeOnline
+
+write-host "Removing users from Azure AD Groups" -ForegroundColor Green
+$MemberID = (Get-AzureADUser -ObjectId $userToOffboard).objectId
+Get-AzureADUserMembership -ObjectId $MemberID -All $true | Where-Object { $_.ObjectType -eq "Group" -and $_.SecurityEnabled -eq $true -and $_.MailEnabled -eq $false } | ForEach-Object {
+    write-host "    Removing using from $($_.displayname)" -ForegroundColor green
+    Remove-AzureADGroupMember -ObjectId $_.ObjectID -MemberId $MemberID
+}
+write-host "Removing users from Unified Groups and Teams" -ForegroundColor Green
+$OffboardingDN = (get-mailbox -Identity $userToOffboard -IncludeInactiveMailbox).DistinguishedName
+Get-Recipient -Filter "Members -eq '$OffboardingDN'" -RecipientTypeDetails 'GroupMailbox' | foreach-object {
+    write-host "    Removing using from $($_.name)" -ForegroundColor green
+    Remove-UnifiedGroupLinks -Identity $_.ExternalDirectoryObjectId -Links $userToOffboard -LinkType Member -Confirm:$false }
+
+write-host "Removing users from Distribution Groups" -ForegroundColor Green
+Get-Recipient -Filter "Members -eq '$OffboardingDN'" | foreach-object {
+    write-host "    Removing using from $($_.name)" -ForegroundColor green
+Remove-DistributionGroupMember -Identity $_.ExternalDirectoryObjectId -Member $OffboardingDN -BypassSecurityGroupManagerCheck -Confirm:$false }
+
+write-host "Setting mailbox to Shared Mailbox" -ForegroundColor Green
+Set-Mailbox $userToOffboard -Type Shared
+write-host "Hiding user from GAL" -ForegroundColor Green
+Set-Mailbox $userToOffboard -HiddenFromAddressListsEnabled $true
+
+# remove inbox rules
+# write-host "Removing inbox rules" -ForegroundColor Green
+# get-inboxrule -Mailbox $Username | fl name,description
+# get-inboxrule -Mailbox $Username | remove-o365-inboxrule
+
+
+#Set Sign in Blocked
+Set-AzureADUser -ObjectId $userToOffboard -AccountEnabled $false
+
+#Disconnect Existing Sessions
+Get-AzureADUser -SearchString $userToOffboard | Revoke-AzureADUserAllRefreshToken
+
+
+write-host "Removing License from user." -ForegroundColor Green
+$AssignedLicensesTable = Get-AzureADUser -ObjectId $userToOffboard | Get-AzureADUserLicenseDetail | Select-Object @{n = "License"; e = { $_.SkuPartNumber } }, skuid
+if ($AssignedLicensesTable) {
+$body = @{
+        addLicenses    = @()
+        removeLicenses = @($AssignedLicensesTable.skuid)
+}
+Set-AzureADUserLicense -ObjectId $userToOffboard -AssignedLicenses $body
+}
+
+write-host "Removed licenses:"
+$AssignedLicensesTable | Export-CSV 'offboard-user-data.csv' -Append -Force 
